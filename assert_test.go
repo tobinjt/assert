07f97dad13c51ed808941261c1bea72987ff2e78
John Tobin <johntobin@johntobin.ie>
2016-07-24 17:29:58 -0400

    Stop exporting NewTestLogger.

--- assert_test.go
+++ assert_test.go
@@ -10,7 +10,7 @@ type testlogger struct {
 	counter int
 }
 
-func NewTestLogger() *testlogger {
+func newTestLogger() *testlogger {
 	return &testlogger{}
 }
 
@@ -20,12 +20,12 @@ func (t *testlogger) Errorf(message string, _ ...interface{}) {
 }
 
 func TestEqual(t *testing.T) {
-	logger := NewTestLogger()
+	logger := newTestLogger()
 	Equal(logger, "message", 7, 23)
 	if 1 != logger.counter {
 		t.Errorf("Equal: Errorf not called for inputs 7, 23\n")
 	}
-	logger = NewTestLogger()
+	logger = newTestLogger()
 	Equal(logger, "message", 42, 42)
 	if 0 != logger.counter {
 		t.Errorf("Equal: Errorf unexpectedly called for inputs 42, 42\n")
@@ -33,12 +33,12 @@ func TestEqual(t *testing.T) {
 }
 
 func TestErrIsNil(t *testing.T) {
-	logger := NewTestLogger()
+	logger := newTestLogger()
 	ErrIsNil(logger, "message", errors.New("A dummy error"))
 	if 1 != logger.counter {
 		t.Error("ErrIsNil: Errorf not called")
 	}
-	logger = NewTestLogger()
+	logger = newTestLogger()
 	ErrIsNil(logger, "message", nil)
 	if 0 != logger.counter {
 		t.Error("ErrIsNil: Errorf unexpectedly called")
@@ -46,17 +46,17 @@ func TestErrIsNil(t *testing.T) {
 }
 
 func TestErrContains(t *testing.T) {
-	logger := NewTestLogger()
+	logger := newTestLogger()
 	ErrContains(logger, "message", nil, "")
 	if 1 != logger.counter {
 		t.Error("ErrContains: Errorf not called for nil error")
 	}
-	logger = NewTestLogger()
+	logger = newTestLogger()
 	ErrContains(logger, "message", errors.New("asdf"), "qwerty")
 	if 1 != logger.counter {
 		t.Error("ErrContains: Errorf not called for bad error message")
 	}
-	logger = NewTestLogger()
+	logger = newTestLogger()
 	ErrContains(logger, "message", errors.New("asdf"), "asd")
 	if 0 != logger.counter {
 		t.Error("ErrContains: Errorf unexpectedly called")
